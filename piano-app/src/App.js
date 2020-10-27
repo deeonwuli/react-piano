7e5bb350787fb9098181f18d3d3b1fbb653d7929
deeonwuli <onwulidumebi@gmail.com>
2020-10-27 15:29:50 +0100

    piano set up

--- piano-app/src/App.js
+++ piano-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
+import AudioPlayer from './keyboard/AudioPlayer'
 
 function App() {
+  const audioPlayer = AudioPlayer()
+
+  useEffect(() => {
+    audioPlayer.setInstrument('acoustic_grand_piano')
+  }, [])
+
+  const handleClick = () => {
+    audioPlayer.playNote('C4')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <button onClick={handleClick}>Playyy</button>
     </div>
   );
 }
 
-export default App;
+export default App
